b68e7122c390aa8972110488a72a57cadef6d1a1
thangnv-0703 <sunshines0703@gmail.com>
2026-07-29 10:13:30 +0700

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        int n = nums.Length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        int[] output = new int[n];
+        prefix[0] = suffix[n - 1] = 1;
+        for (var i = 1; i < n; i++)
+        {
+            prefix[i] = prefix[i - 1] * nums[i - 1];
+        }
+        for (var i = n - 2; i >= 0; i--)
+        {
+            suffix[i] = suffix[i + 1] * nums[i + 1];
+        }
+        for (var i = 0; i < n; i++)
+        {
+            output[i] = prefix[i] * suffix[i];
+        }
+        return output;
+    }
+}
